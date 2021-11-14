18b92818e93977febc41a5ccf574ea9d1156f069
wysiati <64269852+sachin-inguva@users.noreply.github.com>
2021-11-14 18:51:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <div>asdasd</>
+      <div>asdasd</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
