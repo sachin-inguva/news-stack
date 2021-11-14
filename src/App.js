8d9467467a72d578198e271671102da9dde681ba
wysiati <64269852+sachin-inguva@users.noreply.github.com>
2021-11-14 14:37:35 -0500

    try to introduce a bug

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"
       <div>WITH Continuous Integration</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
