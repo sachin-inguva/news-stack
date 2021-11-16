d71b794256f0a721178e3d10d31259b4818fc2ec
wysiati <64269852+sachin-inguva@users.noreply.github.com>
2021-11-16 18:42:46 +0000

    add gh-pages, Header (#7)
    * - add gh-pages
    - cleanup
    - add Header component
    
    * header changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 import 'normalize.css'
 import '@blueprintjs/core/lib/css/blueprint.css'
 import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 
+import { Card } from '@blueprintjs/core'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
-const queryClient = new QueryClient()
+import Header from './Header'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Card style={{ margin: 'auto 50px' }}>
+        <Header />
+      </Card>
     </QueryClientProvider>
   )
 }
