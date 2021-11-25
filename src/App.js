5d4f043f6c3258e24aecc3df61a60789b4041563
SmitaRath <72769273+SmitaRath@users.noreply.github.com>
2021-11-25 01:29:38 -0500

    google news changes (#10)
    Co-authored-by: Smita Rath <smitarath@smitarath-mbp.myfiosgateway.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'normalize.css'
 import '@blueprintjs/core/lib/css/blueprint.css'
 import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 
-import { Card } from '@blueprintjs/core'
+//import { Card } from '@blueprintjs/core'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 import HackerNews from './HackerNews'
-import Header from './Header'
+//import Header from './Header'
+import GoogleNews from './Google/GoogleNews'
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,12 +25,18 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Card>
-        <Header />
+    
+      <Tabs defaultActiveKey="google" id="uncontrolled-tab-example" className="mb-3">
+      <Tab eventKey="google" title="Google News">
+        <GoogleNews />
+      </Tab>
+     <Tab eventKey="hacker" title="Hacker News">
+     <QueryClientProvider client={queryClient}>
         <HackerNews />
-      </Card>
-    </QueryClientProvider>
+        </QueryClientProvider>
+    </Tab>
+    </Tabs>
+   
   )
 }
 
